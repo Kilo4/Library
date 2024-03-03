9a25ac738e929373f73d092ea4084e8a34dede5f
Tomáš Kilik <tomas.kilik@365.bank>
2024-03-03 16:04:46 +0100

    fix errormessage in CalculationRequest

--- src/Dto/Request/CalculationRequest.cs
+++ src/Dto/Request/CalculationRequest.cs
@@ -5,7 +5,7 @@ namespace Webapi.Dto.Request;
 
 public class CalculationRequest
 {
-    [Range(0.01, 100.00, ErrorMessage = "Value must be in the range of 0.01 a 100.00.")]
+    [Range(0.01, 100.00, ErrorMessage = "Value must be in the range of 0.01 and 100.00.")]
     [DefaultValue(54.99)]
     [Required(ErrorMessage = "Input is required")]
     public decimal input { get; set; }
